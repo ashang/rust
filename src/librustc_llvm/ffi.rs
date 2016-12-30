0108656cdb9ececa14ecae2b82bd27ff61722f9f
Jorge Aparicio <japaricious@gmail.com>
2016-12-29 23:28:11 -0500

    sanitizer support

--- src/librustc_llvm/ffi.rs
+++ src/librustc_llvm/ffi.rs
@@ -120,6 +120,9 @@ pub enum Attribute {
     UWTable         = 17,
     ZExt            = 18,
     InReg           = 19,
+    SanitizeThread  = 20,
+    SanitizeAddress  = 21,
+    SanitizeMemory  = 22,
 }
 
 /// LLVMIntPredicate
